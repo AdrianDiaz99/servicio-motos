b43e808cc9f75e1b8eb6f04067d7c89a94d33198
Kairos Jesus Adrian Diaz Orozco <nem.jadrian@coppel.com>
2023-07-17 13:07:56 -0700

    Inicializando proyecto

---
+++ src/main/java/com/app/serviciobicicleta/ServicioBicicletaApplication.java
@@ -0,0 +1,13 @@
+package com.app.serviciobicicleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioBicicletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioBicicletaApplication.class, args);
+	}
+
+}
